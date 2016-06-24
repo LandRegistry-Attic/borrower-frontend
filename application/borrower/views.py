47d981a64809a3dddcbbcbdd0e076c22c9354e39
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-06-24 22:04:37 +0100

    Indentation check

--- application/borrower/views.py
+++ application/borrower/views.py
@@ -43,16 +43,16 @@ def confirm_network_agreement():
         if request.method == "GET":
             return render_template('confirm-borrower-naa.html')
         else:
-        form = request.form
-        if 'validate' in form:
-            agreed_naa = form["agree-naa"]
-            if agreed_naa == False:
-                form.error = "You must agree to these Terms and Conditions to proceed"
-            elif agreed_naa == True:
-                form.error = None
-                return redirect('/mortgage-deed', code=307)
+            form = request.form
+            if 'validate' in form:
+                agreed_naa = form["agree-naa"]
+                if agreed_naa == False:
+                    form.error = "You must agree to these Terms and Conditions to proceed"
+                elif agreed_naa == True:
+                    form.error = None
+                    return redirect('/mortgage-deed', code=307)
+                return render_template('confirm-borrower-naa.html', form=form)
             return render_template('confirm-borrower-naa.html', form=form)
-        return render_template('confirm-borrower-naa.html', form=form)
 
 
 @borrower_landing.route('/verify', methods=['POST'])
