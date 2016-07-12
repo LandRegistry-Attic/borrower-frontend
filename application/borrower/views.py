0a59ee9d087f613cd22b2badbe42528a3269df89
lendmeapound <andy.chan@digital.landregistry.gov.uk>
2016-07-12 17:01:43 +0100

    Updating redirects

--- application/borrower/views.py
+++ application/borrower/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, Response, request, session, redirect
+from flask import Blueprint, render_template, Response, request, session, redirect, url_for
 from application import config
 import logging
 
@@ -22,9 +22,9 @@ def home():
 @borrower_landing.route('/start')
 def start():
     if config.VERIFY:
-        return redirect('/identity-verified', code=307)
+        return redirect(url_for('borrower_landing.identity_verified'), code=307)
     else:
-        return redirect('/borrower-reference', code=307)
+        return redirect(url_for('searchdeed.search_deed_main'), code=307)
 
 
 @borrower_landing.route('/identity-verified', methods=['GET'])
@@ -48,30 +48,30 @@ def verify_identity():
             # Verify has worked, a match was made, but PID cannot now be found. Application fault.
             LOGGER.error("verify-PID-not-found")
             return redirect('/server-error', code=302)
-        return redirect('/identity-verified', code=302)
+        return redirect(url_for('borrower_landing.identity_verified'), code=302)
     else:
         if 'Verify-response-status' in request.headers:
             # Verify has worked, but an error has occurred, depending on status decide what to do
             if "no-match" in request.headers.get('Verify-response-status'):
                 # no-match - Basic no match
                 LOGGER.warn("verify-no-match")
-                return redirect('/verify-no-match', code=302)
+                return redirect(url_for('borrower_landing.verify_no_match'), code=302)
             elif "NoAuthnContext" in request.headers.get('Verify-response-status'):
                 # NoAuthnContext - No authentication context (this is when the user cancels the process)
                 LOGGER.warn("verify-NoAuthnContext")
-                return redirect('/verify-error', code=302)
+                return redirect(url_for('borrower_landing.verify_error'), code=302)
             elif "AuthnFailed" in request.headers.get('Verify-response-status'):
                 # AuthnFailed - Authentication failed
                 LOGGER.warn("verify-AuthnFailed")
-                return redirect('/verify-error', code=302)
+                return redirect(url_for('borrower_landing.verify_error'), code=302)
             elif "Requester" in request.headers.get('Verify-response-status'):
                 # Requester - Requester error (this is when the request is invalid)
                 LOGGER.warn("verify-Requester")
-                return redirect('/verify-error', code=302)
+                return redirect(url_for('borrower_landing.verify_error'), code=302)
             else:
                 # Catch unknown error
                 LOGGER.warn("verify-error-unknown")
-                return redirect('/verify-error', code=302)
+                return redirect(url_for('borrower_landing.verify_error'), code=302)
         else:
             # No PID and No Verify-response-status
             LOGGER.error("no-pid-no-status")
