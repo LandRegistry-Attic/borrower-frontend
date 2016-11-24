d875a9bcacab75e042479496b84075c447259762
treacle74 <ptrelease@gmail.com>
2016-11-24 09:01:10 +0000

    finished unit tests

--- tests/test_finished_template.py
+++ tests/test_finished_template.py
@@ -12,19 +12,18 @@ class TestFinishedTemplate(unittest.TestCase):
     @with_context
     @with_client
     def test_all_signed(self, client):
-        deed_data = {"deed": {"borrowers": [{"signature": 1}, {"signature": 2}]}}
-        html_string = render_template('finished.html', all_signed=check_all_signed(deed_data))
+        html_string = render_template('finished.html', all_signed=True)
         required_string_1 = "The conveyancer will be notified so that they can continue to do the legal work " \
             "needed for your mortgage"
         self.assertIn(required_string_1, html_string)
 
     @with_context
     @with_client
-    def test_one_borrower(self, client):
-        deed_data = {"deed": {"borrowers": [{"signature": 1}]}}
-        html_string = render_template('finished.html', all_signed=check_all_signed(deed_data))
-        required_string_1 = "The conveyancer will be notified so that they can continue to do the legal work " \
-                            "needed for your mortgage"
+    def test_with_unsigned_borrower(self, client):
+        html_string = render_template('finished.html', all_signed=False)
+        required_string_1 = "As you are borrowing money with other people, they will also need to sign the " \
+                            "mortgage deed"
         self.assertIn(required_string_1, html_string)
-
-    # Todo - last test for one or more unsigned borrower
+        required_string_2 = "Once all borrowers have signed the mortgage deed, the conveyancer will be notified " \
+                            "so that they can continue to do the legal work needed for your mortgage"
+        self.assertIn(required_string_2, html_string)
