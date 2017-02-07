0b70d7ce66806adc2d37328e946e9d71a47d52d0
Andy Mantell <andymantell@hotmail.com>
2017-02-07 11:49:35 +0000

    Fix font-awesome issue

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 
 gulp.task('copy', [
+  'clean',
   'copyGov',
   'copyFontAwesome'
 ])
--- gulp/tasks/font-awesome.js
+++ gulp/tasks/font-awesome.js
@@ -5,6 +5,6 @@ var config = require('../config')
 
 gulp.task('copyFontAwesome', ['clean'], function () {
   return gulp
-    .src(path.join(config.assetsPath, 'src/scss/font-awesome/font/**'))
+    .src(path.join(config.assetsPath, 'src/scss/vendor/font-awesome/font/**'))
     .pipe(gulp.dest(path.join(config.assetsPath, 'dist/font')))
 })
