8c4b750c097d2fb1fe73d816b6177f09f54ef9ca
Andy Mantell <andymantell@hotmail.com>
2017-02-14 09:46:59 +0000

    Fix JS linting

--- application/assets/src/javascripts/modules/selection-buttons.js
+++ application/assets/src/javascripts/modules/selection-buttons.js
@@ -1,3 +1,3 @@
 import 'govuk_frontend_toolkit/javascripts/govuk/selection-buttons'
 
-new GOVUK.SelectionButtons('.block-label input[type="radio"], .block-label input[type="checkbox"]')
+new GOVUK.SelectionButtons('.block-label input[type="radio"], .block-label input[type="checkbox"]') // eslint-disable-line no-new,no-undef
