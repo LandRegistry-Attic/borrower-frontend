9d4575a2a92f72841d7bca0298cd88c9001a4f3d
Ramin <ramin.vazir@digital.landregistry.gov.uk>
2017-02-20 09:42:03 +0000

    flake8 changes

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -21,7 +21,8 @@ class TestErrorHandling(unittest.TestCase):
         self.client = app.test_client()
 
     def test_server_error_page(self):
-        expected = 'We are unable to process your request at this time. This was not due to any mistake on your part, but because the service encountered technical issues.'
+        expected = 'We are unable to process your request at this time. This was not due to any mistake on your part,' \
+                   ' but because the service encountered technical issues.'
 
         with app.app_context():
             with app.test_request_context():
@@ -37,7 +38,8 @@ class TestErrorHandling(unittest.TestCase):
         with app.app_context():
             with app.test_request_context():
                 response = self.client.get('/page-not-found')
-                expected = 'We could not find the page you requested. This could be because the address was mistyped, or a link you followed was pointing at a page that no longer exists.'
+                expected = 'We could not find the page you requested. This could be because the address was mistyped, ' \
+                           'or a link you followed was pointing at a page that no longer exists.'
                 soup = BeautifulSoup(response.data)
                 for para in soup.find_all('p'):
                     print(repr(para.text))
