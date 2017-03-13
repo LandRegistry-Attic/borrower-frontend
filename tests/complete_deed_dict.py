f088e2fe4df41c57b4ff6172203bc64e30956812
treacle74 <ptrelease@gmail.com>
2017-03-13 15:38:16 +0000

    shortened lines for pep8

--- tests/complete_deed_dict.py
+++ tests/complete_deed_dict.py
@@ -9,7 +9,9 @@ complete_deed_dict = {
             },
             {
                 "additional_provision_code": "addp002",
-                "description": "This mortgage deed incorporates the Mortgage Conditions (Issue 'J') which have been laid down by the Board of Directors of the lender and of which a copy has been supplied to the borrower."
+                "description": "This mortgage deed incorporates the Mortgage Conditions (Issue 'J') which have been \
+                laid down by the Board of Directors of the lender and of which a copy has been supplied to the \
+                borrower."
             },
             {
                 "additional_provision_code": "addp003",
@@ -17,11 +19,19 @@ complete_deed_dict = {
             },
             {
                 "additional_provision_code": "addp004",
-                "description": "The borrower as a member of the Coventry Building Society will during the existence of this mortgage deed be bound by the Rules of the Society in force from time to time including rules adopted and amendments made after the date of this mortgage deed, except insofar as the Rules are expressly modified by the Mortgage Conditions and this mortgage deed."
+                "description": "The borrower as a member of the Coventry Building Society will during the existence \
+                of this mortgage deed be bound by the Rules of the Society in force from time to time including rules \
+                adopted and amendments made after the date of this mortgage deed, except insofar as the Rules are \
+                expressly modified by the Mortgage Conditions and this mortgage deed."
             },
             {
                 "additional_provision_code": "addp005",
-                "description": "The borrower applies to the Chief Land Registrar to enter the following restriction in the Proprietorship Register of the property: &quot;No disposition of the registered estate by the proprietor of the registered estate or by the proprietor of any registered charge, not being a charge registered before the entry of this restriction, is to be registered without a written consent signed by the proprietor for the time being of the charge dated [the date of this charge] in favour of Coventry Building Society referred to in the Charges Register&quot;."
+                "description": "The borrower applies to the Chief Land Registrar to enter the following restriction \
+                in the Proprietorship Register of the property: &quot;No disposition of the registered estate by the \
+                proprietor of the registered estate or by the proprietor of any registered charge, not being a charge \
+                registered before the entry of this restriction, is to be registered without a written consent signed \
+                by the proprietor for the time being of the charge dated [the date of this charge] in favour of \
+                Coventry Building Society referred to in the Charges Register&quot;."
             }
         ],
         "borrowers": [
@@ -44,13 +54,15 @@ complete_deed_dict = {
         ],
         "charge_clause": {
             "cre_code": "CRE001",
-            "description": "The borrower, with full title guarantee, charges to the lender the property by way of legal mortgage with payment of all money secured by the charge."
+            "description": "The borrower, with full title guarantee, charges to the lender the property by way of \
+            legal mortgage with payment of all money secured by the charge."
         },
         "date_of_mortgage_offer_details": {
             "date_of_mortgage_offer_heading": "Date of Mortgage Offer",
             "date_of_mortgage_offer_value": "a date string"
         },
-        "effective_clause": "This charge takes effect when the registrar receives notification from Land Registry Devices that the charge is to take effect.",
+        "effective_clause": "This charge takes effect when the registrar receives notification from Land Registry \
+        Devices that the charge is to take effect.",
         "lender": {
             "address": "Economic House PO Box 9, High Street Coventry CV1 5QN",
             "name": "COVENTRY BUILDING SOCIETY",
